5ef2f148113a903e1858ee96b9378c76f17a30f7
Zoe Rorvig <zrorvig@gmail.com>
2023-01-02 18:16:11 -0500

    added test items to Engineer.test

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -3,7 +3,27 @@ const Engineer = require("../lib/Engineer");
 const name = "Zoe";
 const id = 2;
 const email = `test@email.com`;
+const github = "ZoeRorvig";
 
 describe("Engineer", () => {
-
+    it(`Grabs github username`, () => {
+        const newEmployee = new Engineer(name, id, email, github);
+        expect(newEmployee.github).toBe(github);
+    });
 });
+
+describe("getGithub", () => {
+    it(`Return github username from getGithub()`, () => {
+        const newEmployee = new Engineer(name, id, email, github);
+        expect(newEmployee.getGithub()).toBe(github);
+    });
+});
+
+describe("getRole", () => {
+    it(`Return role from getRole()`, () => {
+        const role = "Engineer";
+        const newEmployee = new Engineer(name, id, email, github);
+        expect(newEmployee.getRole()).toBe(role);
+    });
+});
+
