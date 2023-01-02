70e681070a69c8a05dae9f1e225189ffb2157c5e
Zoe Rorvig <zrorvig@gmail.com>
2023-01-02 18:28:50 -0500

    added tests to Intern.test

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -3,7 +3,26 @@ const Intern = require("../lib/Intern");
 const name = "Zoe";
 const id = 2;
 const email = `test@email.com`;
+const school = "NC State"
 
 describe("Intern", () => {
+    it(`Grabs school`, () => {
+        const newEmployee = new Intern(name, id, email, school);
+        expect(newEmployee.school).toBe(school);
+    });
+});
 
+describe("getGithub", () => {
+    it(`Return school from getSchool()`, () => {
+        const newEmployee = new Intern(name, id, email, school);
+        expect(newEmployee.getSchool()).toBe(school);
+    });
+});
+
+describe("getRole", () => {
+    it(`Return role from getRole()`, () => {
+        const role = "Intern";
+        const newEmployee = new Intern(name, id, email, school);
+        expect(newEmployee.getRole()).toBe(role);
+    });
 });
