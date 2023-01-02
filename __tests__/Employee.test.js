a4396c0a6a41af2f5d7dc99fd6c54ab72b51e226
Zoe Rorvig <zrorvig@gmail.com>
2023-01-02 13:18:22 -0500

    added initial object test to Employee test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,5 +1,26 @@
 const Employee = require("../lib/Employee");
+const name = "Zoe";
+const id = 2;
+const email = `test@email.com`;
 
 describe("Employee", () => {
+    it(`Creates an employee object`, () => {
+        const newEmployee = new Employee();
+        expect(typeof(newEmployee)).toBe("object");
+    });
 
+    it(`Grabs employee's name`, () => {
+        const newEmployee = new Employee(name);
+        expect(newEmployee.name).toBe(name);
+    });
+
+    it(`Grabs employee's id`, () => {
+        const newEmployee = new Employee(name, id);
+        expect(newEmployee.id).toBe(id);
+    });
+
+    it(`Grabs employee's email`, () => {
+        const newEmployee = new Employee(name, id, email);
+        expect(newEmployee.email).toBe(email);
+    });
 });
