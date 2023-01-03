af03eab2fc9b615a13b0dacdf3f097d4f1d8701c
Zoe Rorvig <zrorvig@gmail.com>
2023-01-02 21:42:41 -0500

    added newManager() and newEngineer()

--- index.js
+++ index.js
@@ -7,45 +7,90 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 const generateTeamMembers = require('./src/page-template');
-const { listenerCount } = require('process');
+// const { listenerCount } = require('process');
 
-function init () {
-    addEmployee();
+employeeArray = [];
+
+function init() {
+    newManager();
 };
 
-function addEmployee () {
+function addEmployee() {
     inquirer.prompt([
         {
-        type: "list",
-        name: "addEmployee",
-        message: "Would you like to add an Employee to your team?",
-        choices: ["Engineer", "Intern", "Finished Adding"],
-         },
-    ])
-    .then(function (input) {
-        if(input.addEmployee === "Engineer") {
-            // addEngineer();
-            console.log("Engineer!");
-        } else if(input.addEmployee === "Intern"){
-            // addIntern();
-            console.log("Intern!");
+            type: "list",
+            name: "addEmployee",
+            message: "Would you like to add an Employee to your team?",
+            choices: ["Engineer", "Intern", "Finished Building my Team"],
+        },
+    ]).then(input => {
+        if (input.addEmployee === "Engineer") {
+            newEngineer();
+        } else if (input.addEmployee === "Intern") {
+            return;
         } else {
             // generateHTML();
-            console.log("HTML!");
+            console.log(employeeArray);
         }
     })
 };
 
-function addManager () {
-
+function newManager() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "managerName",
+            message: `Please enter the manager's name.`,
+        },
+        {
+            type: "input",
+            name: "managerID",
+            message: `Please enter the manager's employee id.`,
+        },
+        {
+            type: "email",
+            name: "managerEmail",
+            message: `Please enter the manager's email.`,
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: `Please enter the manager's office number.`,
+        }
+    ]).then(input => {
+        const newManager = new Manager(input.managerName, input.managerID, input.managerEmail, input.officeNumber);
+        employeeArray.push(newManager);
+        addEmployee();
+    });
 };
 
-function addEngineer () {
-
-};
-
-function addIntern () {
-
+function newEngineer() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "engineerName",
+            message: `Please enter the engineer's name.`,
+        },
+        {
+            type: "input",
+            name: "engineerID",
+            message: `Please enter the engineer's employee id.`,
+        },
+        {
+            type: "email",
+            name: "engineerEmail",
+            message: `Please enter the engineer's email.`,
+        },
+        {
+            type: "input",
+            name: "github",
+            message: `Please enter the engineer's GitHub username.`,
+        }
+    ]).then(input => {
+        const newEngineer = new Engineer(input.engineerName, input.engineerID, input.engineerEmail, input.github);
+        employeeArray.push(newEngineer);
+        addEmployee();
+    });
 };
 
 init();
