a47d001317764c3e30c6961bb30d8e292d25b8c7
Zoe Rorvig <zrorvig@gmail.com>
2023-01-03 23:05:27 -0500

    added some validate statements to prompts

--- index.js
+++ index.js
@@ -5,22 +5,23 @@ const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateTeamMembers = require('./src/pageTemplate.js');
-const pageTemplate = require('./src/pageTemplate.js');
+const generateTeamMembers = require('./src/page-template.js');
+
 
 team = [];
 
-const init = function() {
+const init = function () {
     newManager();
 };
 
-const addEmployee = function() {
+const addEmployee = function () {
     inquirer.prompt([
         {
             type: "list",
             name: "addEmployee",
             message: "Would you like to add an Employee to your team?",
             choices: ["Engineer", "Intern", "Finished Building my Team"],
+            default: "Finished Building my Team",
         },
     ]).then(input => {
         if (input.addEmployee === "Engineer") {
@@ -33,27 +34,31 @@ const addEmployee = function() {
     })
 };
 
-const newManager = function() {
+const newManager = function () {
     inquirer.prompt([
         {
             type: "input",
             name: "managerName",
             message: `Please enter the manager's name.`,
+            validate: nameInput => nameInput ? true : false,
         },
         {
             type: "input",
             name: "managerID",
             message: `Please enter the manager's employee id.`,
+            validate: idInput => idInput ? true : false,
         },
         {
             type: "email",
             name: "managerEmail",
             message: `Please enter the manager's email.`,
+            validate: emailInput => emailInput ? true : false,
         },
         {
             type: "input",
             name: "officeNumber",
             message: `Please enter the manager's office number.`,
+            validate: officeInput => officeInput ? true : false,
         }
     ]).then(input => {
         const newManager = new Manager(input.managerName, input.managerID, input.managerEmail, input.officeNumber);
@@ -62,27 +67,31 @@ const newManager = function() {
     });
 };
 
-const newEngineer = function() {
+const newEngineer = function () {
     inquirer.prompt([
         {
             type: "input",
             name: "engineerName",
             message: `Please enter the engineer's name.`,
+            validate: nameInput => nameInput ? true : false,
         },
         {
             type: "input",
             name: "engineerID",
             message: `Please enter the engineer's employee id.`,
+            validate: idInput => idInput ? true : false,
         },
         {
             type: "email",
             name: "engineerEmail",
             message: `Please enter the engineer's email.`,
+            validate: emailInput => emailInput ? true : false,
         },
         {
             type: "input",
             name: "github",
             message: `Please enter the engineer's GitHub username.`,
+            validate: githubInput => githubInput ? true : false,
         }
     ]).then(input => {
         const newEngineer = new Engineer(input.engineerName, input.engineerID, input.engineerEmail, input.github);
@@ -91,27 +100,31 @@ const newEngineer = function() {
     });
 };
 
-const newIntern = function() {
+const newIntern = function () {
     inquirer.prompt([
         {
             type: "input",
             name: "internName",
             message: `Please enter the intern's name.`,
+            validate: nameInput => nameInput ? true : false,
         },
         {
             type: "input",
             name: "internID",
             message: `Please enter the intern's employee id.`,
+            validate: idInput => idInput ? true : false,
         },
         {
             type: "email",
             name: "internEmail",
             message: `Please enter the intern's email.`,
+            validate: emailInput => emailInput ? true : false,
         },
         {
             type: "input",
             name: "school",
             message: `Please enter the intern's school.`,
+            validate: schoolInput => schoolInput ? true : false,
         }
     ]).then(input => {
         const newIntern = new Intern(input.internName, input.internID, input.internEmail, input.school);
@@ -120,9 +133,9 @@ const newIntern = function() {
     });
 };
 
-const generateHTML = function(){
-    fs.writeFile('./dist/team.html', pageTemplate(team),(err) =>
-    err ? console.error(err) : console.log(`Successfully generated page!`)
+const generateHTML = function () {
+    fs.writeFile('./dist/team.html', generateTeamMembers(team), (err) =>
+        err ? console.error(err) : console.log(`Successfully generated page!`)
     );
 };
 
--- src/page-template.js
+++ src/page-template.js
@@ -56,54 +56,56 @@ const generateIntern = (intern) => {
 
 // export function to generate entire page
 module.exports = (team) => {
-   return `<!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Team Profile</title>
-      <!-- Minified version -->
-      <style>
-        @import "https://cdn.simplecss.org/simple.min.css";
-    
-        main {
-          display: grid;
-          grid-column: 1/-1;
-          justify-items: center;
-          grid-template-columns: 1fr 1fr 1fr;
-          gap: 1rem;
-          max-width: 1140px;
-          margin: auto;
-        }
-    
-        article {
-          background-color: rgb(1, 39, 39);
-        }
-    
-        @media screen and (max-width: 1140px) {
-          main {
-            grid-template-columns: 1fr 1fr;
-          }
-        }
-        @media screen and (max-width: 720px) {
-          main {
-            grid-template-columns: 1fr;
-          }
-        }
-      </style>
-    </head>
-    <body>
-      <header>
-        <h1>My Team</h1>
-      </header>
-      <main>
-        ${generateTeamMembers(team)}
-      </main>
-      <footer>
-        &copy; 2022-2023
-      </footer>
-    </body>
-    </html>
-    `
+return `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Team Profile</title>
+  <!-- Minified version -->
+  <style>
+    @import "https://cdn.simplecss.org/simple.min.css";
+
+    main {
+      display: grid;
+      grid-column: 1/-1;
+      justify-items: center;
+      grid-template-columns: 1fr 1fr 1fr;
+      gap: 1rem;
+      max-width: 1140px;
+      margin: auto;
+    }
+
+    article {
+      background-color: rgb(1, 39, 39);
+      margin: 10px;
+      width: 100%;
+    }
+
+    @media screen and (max-width: 1140px) {
+      main {
+        grid-template-columns: 1fr 1fr;
+      }
+    }
+    @media screen and (max-width: 720px) {
+      main {
+        grid-template-columns: 1fr;
+      }
+    }
+  </style>
+</head>
+<body>
+  <header>
+    <h1>My Team</h1>
+  </header>
+  <main>
+    ${generateTeamMembers(team)}
+  </main>
+  <footer>
+    &copy; 2022-2023
+  </footer>
+</body>
+</html>
+`
 };
